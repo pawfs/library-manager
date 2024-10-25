4fb8dd362eb58481f7129123de30b73ab1e1eb34
pawfs <bahriayavx@gmail.com>
2024-10-25 17:12:32 +0100

    Add book list and details pages

--- src/Controller/Admin/BookController.php
+++ src/Controller/Admin/BookController.php
@@ -1,9 +1,13 @@
 <?php
 
 namespace App\Controller\Admin;
+
 use App\Entity\Book;
 use App\Form\BookType;
+use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,30 +16,44 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/admin/book')]
 class BookController extends AbstractController
 {
-    #[Route('', name: 'app_admin_book_index', methods: ['GET'])]
-    public function index(): Response
-    {
-        return $this->render('admin/book/index.html.twig', [
-            'controller_name' => 'BookController',
-        ]);
+  #[Route('', name: 'app_admin_book_index', methods: ['GET'])]
+  public function index(Request $request, BookRepository $bookRepository): Response
+  {
+    $books = Pagerfanta::createForCurrentPageWithMaxPerPage(
+      new QueryAdapter($bookRepository->createQueryBuilder('b')),
+      $request->query->get('page', 1),
+      20,
+    );
+    return $this->render('admin/book/index.html.twig', [
+      'books' => $books,
+    ]);
+  }
+
+  #[Route('/new', name: 'app_admin_book_new', methods: ['GET', 'POST'])]
+  #[Route('/{id}/edit', name: 'app_admin_book_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+  public function new(?Book $book, Request $request, EntityManagerInterface $manager): Response
+  {
+    $book ??= new Book();
+    $form = $this->createForm(BookType::class, $book);
+
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $manager->persist($book);
+      $manager->flush();
+
+      return $this->redirectToRoute('app_admin_book_index');
     }
 
-    #[Route('/new', name: 'app_admin_book_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $manager): Response
-    {
-        $book = new Book();
-        $form = $this->createForm(BookType::class, $book);
+    return $this->render('admin/book/new.html.twig', [
+      'form' => $form,
+    ]);
+  }
 
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $manager->persist($book);
-            $manager->flush();
-
-            return $this->redirectToRoute('app_admin_book_new');
-        }
-
-        return $this->render('admin/book/new.html.twig', [
-            'form' => $form,
-        ]);
-    }
+  #[Route('/{id}', name: 'app_admin_book_show', requirements: ['id' => '\d+'], methods: ['GET'])]
+  public function show(?Book $book): Response
+  {
+    return $this->render('admin/book/show.html.twig', [
+      'book' => $book,
+    ]);
+  }
 }

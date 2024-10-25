316b099cced57a37ae97a2e5685efbb23bbbb88d
pawfs <bahriayavx@gmail.com>
2024-10-25 13:57:54 +0100

    Add find by date of birth

--- src/Controller/Admin/AuthorController.php
+++ src/Controller/Admin/AuthorController.php
@@ -15,12 +15,23 @@ use Symfony\Component\Routing\Attribute\Route;
 class AuthorController extends AbstractController
 {
   #[Route('', name: 'app_admin_author_index', methods: ['GET'])]
-  public function index(AuthorRepository $authorRepository): Response
+  public function index(Request $request, AuthorRepository $authorRepository): Response
   {
+    // $dates = [];
+    // if ($request->query->has('start')) {
+    //   $dates['start'] = $request->query->get('start');
+    // }
+
+    // if ($request->query->has('end')) {
+    //   $dates['end'] = $request->query->get('end');
+    // }
+
+    // $authors = $authorRepository->findByDateOfBirth($dates);
     $authors = $authorRepository->findAll();
 
+
     return $this->render('admin/author/index.html.twig', [
-      'controller_name' => 'AuthorController',
+
       'authors' => $authors,
     ]);
   }
@@ -43,11 +54,13 @@ class AuthorController extends AbstractController
     ]);
   }
 
-  #[Route('/{id}', name: 'app_admin_author_show', requirements: ['id' => '\d+'] , methods: ['GET'])]
+  #[Route('/{id}', name: 'app_admin_author_show', requirements: ['id' => '\d+'], methods: ['GET'])]
   public function show(?Author $author): Response
   {
     return $this->render('admin/author/show.html.twig', [
       'author' => $author,
     ]);
   }
+
+
 }

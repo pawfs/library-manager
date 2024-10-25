11318416f33f76c9e5dfd461c561fffffe2db16d
pawfs <bahriayavx@gmail.com>
2024-10-25 10:20:04 +0100

    Add editor form validation

--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -13,110 +13,109 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[ORM\Entity(repositoryClass: AuthorRepository::class)]
 class Author
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column]
+  private ?int $id = null;
 
-    #[Assert\Length(min: 10)]
-    #[Assert\NotBlank]
-    #[ORM\Column(length: 255)]
-    private ?string $name = null;
+  #[Assert\NotBlank]
+  #[ORM\Column(length: 255)]
+  private ?string $name = null;
 
-    #[Assert\NotBlank()]
-    #[ORM\Column()]
-    private ?\DateTimeImmutable $dateOfBirth = null;
+  #[Assert\NotBlank()]
+  #[ORM\Column()]
+  private ?\DateTimeImmutable $dateOfBirth = null;
 
-    #[Assert\GreaterThan(propertyPath: 'dateOfBirth')]
-    #[ORM\Column(nullable: true)]
-    private ?\DateTimeImmutable $dateOfDeath = null;
+  #[Assert\GreaterThan(propertyPath: 'dateOfBirth')]
+  #[ORM\Column(nullable: true)]
+  private ?\DateTimeImmutable $dateOfDeath = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $nationality = null;
+  #[ORM\Column(length: 255, nullable: true)]
+  private ?string $nationality = null;
 
-    #[ORM\ManyToMany(targetEntity: Book::class, inversedBy: 'authors')]
-    private Collection $books;
+  #[ORM\ManyToMany(targetEntity: Book::class, inversedBy: 'authors')]
+  private Collection $books;
 
-    public function __construct()
-    {
-        $this->books = new ArrayCollection();
+  public function __construct()
+  {
+    $this->books = new ArrayCollection();
+  }
+
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+
+  public function getName(): ?string
+  {
+    return $this->name;
+  }
+
+  public function setName(string $name): static
+  {
+    $this->name = $name;
+
+    return $this;
+  }
+
+  public function getDateOfBirth(): ?\DateTimeImmutable
+  {
+    return $this->dateOfBirth;
+  }
+
+  public function setDateOfBirth(\DateTimeImmutable $dateOfBirth): static
+  {
+    $this->dateOfBirth = $dateOfBirth;
+
+    return $this;
+  }
+
+  public function getDateOfDeath(): ?\DateTimeImmutable
+  {
+    return $this->dateOfDeath;
+  }
+
+  public function setDateOfDeath(?\DateTimeImmutable $dateOfDeath): static
+  {
+    $this->dateOfDeath = $dateOfDeath;
+
+    return $this;
+  }
+
+  public function getNationality(): ?string
+  {
+    return $this->nationality;
+  }
+
+  public function setNationality(?string $nationality): static
+  {
+    $this->nationality = $nationality;
+
+    return $this;
+  }
+
+  /**
+   * @return Collection<int, Book>
+   */
+  public function getBooks(): Collection
+  {
+    return $this->books;
+  }
+
+  public function addBook(Book $book): static
+  {
+    if (!$this->books->contains($book)) {
+      $this->books->add($book);
+      $book->addAuthor($this);
     }
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+    return $this;
+  }
 
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
+  public function removeBook(Book $book): static
+  {
+    $this->books->removeElement($book);
 
-    public function setName(string $name): static
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getDateOfBirth(): ?\DateTimeImmutable
-    {
-        return $this->dateOfBirth;
-    }
-
-    public function setDateOfBirth(\DateTimeImmutable $dateOfBirth): static
-    {
-        $this->dateOfBirth = $dateOfBirth;
-
-        return $this;
-    }
-
-    public function getDateOfDeath(): ?\DateTimeImmutable
-    {
-        return $this->dateOfDeath;
-    }
-
-    public function setDateOfDeath(?\DateTimeImmutable $dateOfDeath): static
-    {
-        $this->dateOfDeath = $dateOfDeath;
-
-        return $this;
-    }
-
-    public function getNationality(): ?string
-    {
-        return $this->nationality;
-    }
-
-    public function setNationality(?string $nationality): static
-    {
-        $this->nationality = $nationality;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Book>
-     */
-    public function getBooks(): Collection
-    {
-        return $this->books;
-    }
-
-    public function addBook(Book $book): static
-    {
-        if (!$this->books->contains($book)) {
-            $this->books->add($book);
-            $book->addAuthor($this);
-        }
-
-        return $this;
-    }
-
-    public function removeBook(Book $book): static
-    {
-        $this->books->removeElement($book);
-
-        return $this;
-    }
+    return $this;
+  }
 }

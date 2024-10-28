d436deade5cd5e8dd4d5cb94116245f66389f7af
pawfs <bahriayavx@gmail.com>
2024-10-28 12:29:12 +0100

    Update user entity's properties

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,104 +6,163 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_USERNAME', fields: ['username'])]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column]
+  private ?int $id = null;
 
-    #[ORM\Column(length: 180)]
-    private ?string $username = null;
+  #[ORM\Column(length: 180, nullable: true)]
+  private ?string $username = null;
 
-    /**
-     * @var list<string> The user roles
-     */
-    #[ORM\Column]
-    private array $roles = [];
+  /**
+   * @var list<string> The user roles
+   */
+  #[ORM\Column]
+  private array $roles = [];
 
-    /**
-     * @var string The hashed password
-     */
-    #[ORM\Column]
-    private ?string $password = null;
+  /**
+   * @var string The hashed password
+   * The Regex constraint is used to enforce a strong password:
+   * * At least one digit
+   * * At least one lower case letter
+   * * At least one upper case letter
+   * * At least one special character
+   * * At least 8 characters long
+   */
+  #[Assert\NotCompromisedPassword()]
+  #[Assert\PasswordStrength(minScore: Assert\PasswordStrength::STRENGTH_STRONG)]
+  #[Assert\Regex('/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.*\s).{8,32}$/')]
+  #[ORM\Column]
+  private ?string $password = null;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  #[Assert\Email()]
+  #[Assert\NotBlank()]
+  #[ORM\Column(length: 255, unique: true)]
+  private ?string $email = null;
 
-    public function getUsername(): ?string
-    {
-        return $this->username;
-    }
+  #[Assert\NotBlank()]
+  #[ORM\Column(length: 255)]
+  private ?string $firstName = null;
 
-    public function setUsername(string $username): static
-    {
-        $this->username = $username;
+  #[Assert\NotBlank()]
+  #[ORM\Column(length: 255)]
+  private ?string $lastName = null;
 
-        return $this;
-    }
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
 
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
-    public function getUserIdentifier(): string
-    {
-        return (string) $this->username;
-    }
+  public function getUsername(): ?string
+  {
+    return $this->username;
+  }
 
-    /**
-     * @see UserInterface
-     *
-     * @return list<string>
-     */
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+  public function setUsername(string $username): static
+  {
+    $this->username = $username;
 
-        return array_unique($roles);
-    }
+    return $this;
+  }
 
-    /**
-     * @param list<string> $roles
-     */
-    public function setRoles(array $roles): static
-    {
-        $this->roles = $roles;
+  /**
+   * A visual identifier that represents this user.
+   *
+   * @see UserInterface
+   */
+  public function getUserIdentifier(): string
+  {
+    return (string) $this->email;
+  }
 
-        return $this;
-    }
+  /**
+   * @see UserInterface
+   *
+   * @return list<string>
+   */
+  public function getRoles(): array
+  {
+    $roles = $this->roles;
+    // guarantee every user at least has ROLE_USER
+    $roles[] = 'ROLE_USER';
 
-    /**
-     * @see PasswordAuthenticatedUserInterface
-     */
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
+    return array_unique($roles);
+  }
 
-    public function setPassword(string $password): static
-    {
-        $this->password = $password;
+  /**
+   * @param list<string> $roles
+   */
+  public function setRoles(array $roles): static
+  {
+    $this->roles = $roles;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    /**
-     * @see UserInterface
-     */
-    public function eraseCredentials(): void
-    {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
-    }
+  /**
+   * @see PasswordAuthenticatedUserInterface
+   */
+  public function getPassword(): ?string
+  {
+    return $this->password;
+  }
+
+  public function setPassword(string $password): static
+  {
+    $this->password = $password;
+
+    return $this;
+  }
+
+  /**
+   * @see UserInterface
+   */
+  public function eraseCredentials(): void
+  {
+    // If you store any temporary, sensitive data on the user, clear it here
+    // $this->plainPassword = null;
+  }
+
+  public function getEmail(): ?string
+  {
+    return $this->email;
+  }
+
+  public function setEmail(string $email): static
+  {
+    $this->email = $email;
+
+    return $this;
+  }
+
+  public function getFirstName(): ?string
+  {
+    return $this->firstName;
+  }
+
+  public function setFirstName(string $firstName): static
+  {
+    $this->firstName = $firstName;
+
+    return $this;
+  }
+
+  public function getLastName(): ?string
+  {
+    return $this->lastName;
+  }
+
+  public function setLastName(string $lastName): static
+  {
+    $this->lastName = $lastName;
+
+    return $this;
+  }
 }
